2a38be75320e3f8301d7270575e838079b00d167
yolotouch <ungchan052804@gmail.com>
2024-06-14 00:18:14 +0900

    Migrated database to mongodb

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -99,10 +99,20 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': '24x365',
+        'ENFORCE_SCHEMA': False,
+        'CLIENT': {
+            'host': 'localhost',
+            'port': 27017,
+            'username': 'admin',
+            'password': 'test1234',
+            'authSource': '24x365',
+            'authMechanism': 'SCRAM-SHA-256',
+        },
     }
 }
+
 
 
 # Password validation
