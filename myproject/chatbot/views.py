37f7f9a3e4db90e42a6f1c440ea7f6b5ec5707ea
lim-4158 <129579903+lim-4158@users.noreply.github.com>
2024-06-27 16:47:37 +0800

    made chatbot time aware

--- myproject/chatbot/views.py
+++ myproject/chatbot/views.py
@@ -6,6 +6,7 @@ from rest_framework.response import Response
 from .serializers import ChatResponseSerializer
 import openai
 import os
+from datetime import datetime
 
 # Set your OpenAI API key here
 openai.api_key = os.getenv('OPEN_AI_KEY')
@@ -23,13 +24,16 @@ def get_response(request):
     
     conversation = request.data.get('messages', [])
 
+    timenow = datetime.now()
+    daynow = datetime.now().strftime("%A")
 
-    formatted_messages = [{"role": "system", "content": """You are a part of a larger operation to help with time management to help user add events to their google calendar. Your task is to understand and extract information from the user. 
+
+    formatted_messages = [{"role": "system", "content": f"""The time now is {timenow} and it is {daynow}.You are a part of a larger operation to help with time management to help user add events to their google calendar. Your task is to understand and extract information from the user. 
         You need to get information on the type of event and also the time of event. output in the following format 'type: xxx, time: xxx'. 
         The type of event is either ADD or DELETE. It means adding or deleting events. When I tell you that i got something going on without mentioning deletion, it would mean to add event. the default would be to add. 
         Always ask for user confirmation after you retrieved the data. 
         When you are unsure of what the user wants, for example when the user say next wednesday but it could be ambiguous, clarify with the user. 
-        Once clarified, produce a json file.
+        Once clarified, produce a json file with 3 fields, summary, start date and time, end date and time.
     """}]
 
     for msg in conversation:
@@ -37,7 +41,7 @@ def get_response(request):
         formatted_messages.append({"role": role, "content": msg['text']})
 
     response = openai.chat.completions.create(
-        model="gpt-3.5-turbo",
+        model="gpt-3.5-turbo", #gpt-4o #0125
         messages=formatted_messages,
         max_tokens=150
     )
