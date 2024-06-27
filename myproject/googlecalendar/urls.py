883ac270128e936ab5d2e72e48efad98ea2fad98
yolotouch <148688315+yolotouch@users.noreply.github.com>
2024-06-27 18:58:56 +0900

    Modified URLs

--- myproject/googlecalendar/urls.py
+++ myproject/googlecalendar/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from .views import google_calendar_events, oauth2callback, create_event, force_reauthentication, get_user_calendar_id, check_google_auth
 
 urlpatterns = [
-    path("calendar/events/", google_calendar_events, name="google-calendar-events"),
+    path("calendar/events", google_calendar_events, name="google-calendar-events"),
     path("googlecalendar/oauth2callback", oauth2callback, name="oauth2callback"),
-    path("calendar/events/create/", create_event, name="create-event"),
-    path("googlecalendar/force_reauth", force_reauthentication, name="force-reauthentication"),
+    path("calendar/createevents", create_event, name="create-event"),
+    path("calendar/force_reauth", force_reauthentication, name="force-reauthentication"),
     path("calendar/displayevents", get_user_calendar_id, name="display-events"),
     path("calendar/checkauth", check_google_auth, name="check-auth")
 ]
